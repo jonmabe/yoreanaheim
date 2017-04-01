e483fa9e61629e8e6bcc16d246bb1d2fe379a6b0
Jonathan Mabe <jonmabe@gmail.com>
2017-04-01 16:38:29 -0700

    clean up layout

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { browserHistory, Router, Route, IndexRoute, Link, withRouter } from 'react-router'
 import PublicationItem from './PublicationItem.jsx';
 
-export default class Layout extends React.Component {  
+export default class Layout extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
 		};
 	}
-    
+
     componentWillMount() {
         const typekitScript = document.createElement('script');
         typekitScript.src = 'https://use.typekit.net/knf7vtt.js';
@@ -22,37 +22,37 @@ export default class Layout extends React.Component {
 	componentWillUnmount() {
 	}
 
-    render() {    
+    render() {
          return (
-            <div className="container container-fluid">
+            <div className="container container-fluid" style={{marginTop:'10px'}}>
                 <div className="row">
-                    <div className="col-lg-4">
-                        <div className="row">
-                            <h1 className="sr-only">Yore Anaheim</h1>
-                             <Link to={'/'}><img className="img-fluid" src={require('../assets/header-685x518.jpg')} /></Link>
+                    <div className="col-4">
+                        <div className="row" style={{marginBottom:'10px'}}>
+				                    <div className="col">
+	                            <h1 className="sr-only">Yore Anaheim</h1>
+	                             <Link to={'/'}><img className="img-fluid" src={require('../assets/header-685x518.jpg')} /></Link>
+													 </div>
                         </div>
                         <div className="row">
-                            <ul className="nav">
-                                <li className="nav-item">
-                                    <Link to={'/'} className="nav-link">Publications</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to={'/about'} className="nav-link">About</Link>
-                                </li>
-                            </ul>
+				                    <div className="col">
+	                            <div className="list-group">
+																	<Link to={'/'} className="list-group-item list-group-item-action">Publications</Link>
+	                                <Link to={'/about'} className="list-group-item list-group-item-actionk">About</Link>
+	                            </div>
+														</div>
                         </div>
                     </div>
-                    <div className="col-lg-8">
+                    <div className="col-8">
                         {this.props.children}
                     </div>
                 </div>
                 <div className="row">&nbsp;</div>
                 <div className="row">
-                    <div className="col-lg-12 text-xs-center">
-                        Copyright &copy; 2016 Jason Schultz, Jonathan Mabe, Chris Maya 
+                    <div className="col-12 text-xs-center">
+                        Copyright &copy; 2016 Jason Schultz, Jonathan Mabe, Chris Maya
                     </div>
                 </div>
             </div>
          )
-    } 
+    }
 }
