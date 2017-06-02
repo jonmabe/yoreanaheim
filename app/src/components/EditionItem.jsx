eab06b7bb7e31e67ea34b5e2d37b30c71f2c0811
Jonathan Mabe <jonmabe@gmail.com>
2017-06-02 15:41:56 -0700

    This is what happens when your local dev env doesn’t work

--- app/src/components/EditionItem.jsx
+++ app/src/components/EditionItem.jsx
@@ -7,7 +7,7 @@ export default class EditionItem extends React.Component {
 		super(props);
 	}
   render() {
-		var editionName = dateFormat(his.props.edition.edition_date.replace('Z', ''), 'mmmm dd, yyyy (dddd)');
+		var editionName = dateFormat(this.props.edition.edition_date.replace('Z', ''), 'mmmm dd, yyyy (dddd)');
   	return <a href={`${this.props.edition.pdf}`} target='_blank' className="list-group-item list-group-item-action justify-content-between">
 			{editionName}
 			<span className="badge badge-default badge-pill">{this.props.edition.pages.toLocaleString()}</span>
